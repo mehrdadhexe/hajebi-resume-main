e48e6d1fa4771f763f6da1f9920c3933915bf565
mehrdadhexe <mehrdad.h.exe@gmail.com>
2019-01-21 01:33:07 +0330

    Updates

---
+++ precache-manifest.84ddc60fd270dcde1e13c565284f05ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hajebi-resume-main/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "f5a6c6dcf92ae4e82353",
+    "url": "/hajebi-resume-main/static/js/runtime~main.f5a6c6dc.js"
+  },
+  {
+    "revision": "86b334222f62de62f692",
+    "url": "/hajebi-resume-main/static/js/main.86b33422.chunk.js"
+  },
+  {
+    "revision": "1e2104dcb1dc3abbc30c",
+    "url": "/hajebi-resume-main/static/js/1.1e2104dc.chunk.js"
+  },
+  {
+    "revision": "86b334222f62de62f692",
+    "url": "/hajebi-resume-main/static/css/main.0d90b7db.chunk.css"
+  },
+  {
+    "revision": "a090caee8e2156e10c6e4209281bd61a",
+    "url": "/hajebi-resume-main/index.html"
+  }
+];
